ee78f216aa1a030f2a5ce0b50837fee9ce2d44a8
Mustafa Erkan <bilgi>
2020-04-07 12:28:21 +0300

    Initial Commit

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by Mustafa Erkan on 7.04.2020.
+//  Copyright © 2020 Mustafa Erkan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
